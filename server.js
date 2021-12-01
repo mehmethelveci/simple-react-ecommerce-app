84e47ea3bfdf8ccfd7af0a0eec10566a3bdc2840
Mehmet Helveci <mehmethelveci@gmail.com>
2021-12-01 14:54:49 +0300

    server.js changes

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults({ static: "./build" });
-const port = process.env.PORT || 3000;
+const jsonServer = require('json-server');
+const app = jsonServer.create();
+const path = require('path');
+const express = require('express');
+const middlewares = jsonServer.defaults();
+const router = jsonServer.router('db.json');
+const port = process.env.PORT || 3001;
 
-server.use(middlewares);
-server.use(router);
+app.use('/db', middlewares, router);
+app.use(express.static(path.join(__dirname, 'build')));
 
-server.listen(port);
+app.get('/*', function(req, res) {
+   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port);
